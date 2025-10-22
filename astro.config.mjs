699e5e75a52497cc1df1f2a847b1024ee4e239ed
Carl Uebelhart <carl@erutan.net>
2025-10-22 16:08:40 -0700

    add astro-compress, some other build process optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   srcDir: './src-astro',
   outDir: './dist',
   publicDir: './public',
-  integrations: [mdx()],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler',
-          loadPaths: [
-            path.join(__dirname, 'node_modules/foundation-sites/scss'),
-            path.join(__dirname, 'node_modules/motion-ui/src'),
-            path.join(__dirname, 'node_modules')
-          ],
-          silenceDeprecations: ['import']
-        }
-      }
-    }
-  }
+  integrations: [
+    mdx(),
+    compress({
+      CSS: true,
+      HTML: true,
+      Image: true,
+      JavaScript: true,
+      SVG: true,
+    })
+  ]
 });
